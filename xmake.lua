001606641f007c61c3120e152c7b84b3bfca9199
Zoltan Majoros <zoltan@arcanelab.com>
2025-02-11 00:10:39 +0100

    Added debug mode

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,17 @@ set_version("1.0.0")
 
 set_languages("c++20")
 
+if is_mode("debug") then
+    add_defines("DEBUG")
+    set_symbols("debug")
+    set_optimize("none")
+else 
+    set_symbols("hidden")
+    -- optionally we can include debug symbols in release builds, useful for profiling
+    -- set_symbols("debug")
+    set_optimize("fastest")
+end
+
 target("Mano")
     set_kind("binary")
     set_targetdir("bin")
