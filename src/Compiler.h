1bf36fbc156d9f53241945c3a58f392f508f47ce
Zoltan Majoros <zoltan@arcanelab.com>
2025-02-11 03:38:49 +0100

    Token types are now displayed in text instead

--- src/Compiler.h
+++ src/Compiler.h
@@ -1,8 +1,22 @@
 #include <Lexer.h>
 #include <Parser.h>
-
 #include <string>
 #include <iostream>
+
+// Helper function to convert TokenType to string.
+std::string tokenTypeToString(Arcanelab::Mano::TokenType type) {
+    switch (type) {
+        case Arcanelab::Mano::TokenType::Identifier:    return "Identifier";
+        case Arcanelab::Mano::TokenType::Keyword:       return "Keyword";
+        case Arcanelab::Mano::TokenType::Number:        return "Number";
+        case Arcanelab::Mano::TokenType::String:        return "String";
+        case Arcanelab::Mano::TokenType::Operator:      return "Operator";
+        case Arcanelab::Mano::TokenType::Punctuation:   return "Punctuation";
+        case Arcanelab::Mano::TokenType::EndOfFile:     return "EndOfFile";
+        case Arcanelab::Mano::TokenType::Unknown:       return "Unknown";
+        default:                                       return "Unknown";
+    }
+}
 
 namespace Arcanelab::Mano
 {
@@ -19,16 +33,17 @@ namespace Arcanelab::Mano
 
             for (auto&& token : tokens)
             {
-                std::cout << "Token Type: " << static_cast<int>(token.type)
+                // Here we print the token type using the helper function.
+                std::cout << "Token Type: " << tokenTypeToString(token.type)
                     << " | Lexeme: [" << token.lexeme << "]"
                     << " | Line: " << token.line
                     << " | Column: " << token.column << "\n";        
             }
-        
+
             Parser parser(tokens);
             parser.ParseProgram();
         }
     private:
-        
+        // Other members if needed.
     };
-} // namespace
+} // namespace Arcanelab::Mano
